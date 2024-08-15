d839b41438617b042e93dc14796bda3441019173
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-08-15 17:06:49 +0500

    fetch code practice added

--- callBackPractice.js
+++ callBackPractice.js
@@ -21,3 +21,40 @@
 // foo(callBack)
 
 // console.log("ma phele chalo ga")
+
+// -------------------------------- Promise Practice -------------
+
+// function pro() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             dataObj = {
+//                 name: "jawad",
+//                 email: "muhammadjawad.0@gmail.com",
+//                 profile: "picture.com",
+//                 password: "jawad123",
+//             };
+//             resolve(dataObj)
+//             // reject("error agya ha")
+//         }, 3000)
+//     })
+// }
+
+// pro()
+
+//     .then((data) => {
+//         console.log("data>>>>", data)
+//     }).catch((err) => {
+//         console.log("err >>", err)
+//     })
+
+// ------------------- fetch bhi promise return krta ha ----------------------
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((data) => {
+        return data.json()
+    })
+    .then(function (data) {
+        console.log(data, "<<< data")
+    }).catch(function (err) {
+        console.log(err, "<<< err")
+    })
